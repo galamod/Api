6baae94d276fadf2b07be38b08bfabfdf82963a7
Admin <Admin@DEVOPS>
2025-04-29 22:28:14 +0300

    Update

---
+++ PingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PingController : ControllerBase
+    {
+        /// <summary>
+        /// Проверка, что сервер работает
+        /// </summary>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Server is alive");
+        }
+    }
+}
