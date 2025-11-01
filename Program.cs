493ab7a7038032f60885e430521209479d159662
Admin <Admin@DEVOPS>
2025-11-01 22:25:52 +0300

    Fix

--- Program.cs
+++ Program.cs
@@ -43,7 +43,7 @@ builder.Services.AddCors(options =>
             .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod()
-            .AllowCredentials();
+/*            .AllowCredentials()*/;
     });
 });
 
